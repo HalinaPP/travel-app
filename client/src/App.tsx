53ab233c5f95c11dc74183a21be0bb93a0e294f2
Iogsotot <ajustusa@gmail.com>
2021-03-09 01:52:57 +0300

    fead: add slider & styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Country from './components/Country';
 import CountriesList from './components/CountriesList';
 import Footer from './components/Footer';
--- client/src/components/CountriesList/CountriesList.tsx
+++ client/src/components/CountriesList/CountriesList.tsx
@@ -1,6 +1,7 @@
 import './countriesList.scss';
 import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
+import Carousel from 'react-elastic-carousel';
 // import promoBg from '../../assets/images/ireland.jpg';
 import heartIcon from '../../assets/icons/heart.png';
 
@@ -31,12 +32,87 @@ const CountriesList: FC = () => (
       </div>
     </section>
     <section className='countries'>
-      <a href='#' className='btn btn--ghost'>
-        <img src={heartIcon} alt="♡" className='icon'/>
-        Choose your Favorite
-      </a>
-      <div className="slider">
-        
+      <div className='wrapper'>
+        <a href='#' className='btn btn--ghost'>
+          <img src={heartIcon} alt='♡' className='icon' />
+          Choose your Favorite
+        </a>
+        <div className='slider'>
+          <Carousel itemsToScroll={1} itemsToShow={3} isRTL={false} pagination={false}>
+            <div className='slide'>
+              <div className='country-card'>
+                <div className='image-block'>
+                  <div className='image'></div>
+                  <div className='image-outline'></div>
+                </div>
+                <div className='title'>Georgia</div>
+                <div className='subtitle'>Tbilisi</div>
+              </div>
+              <div className='country-card'>
+                <div className='image-block'>
+                  <div className='image'></div>
+                  <div className='image-outline'></div>
+                </div>
+                <div className='title'>Georgia</div>
+                <div className='subtitle'>Tbilisi</div>
+              </div>
+            </div>
+            <div className='slide'>
+              <div className='country-card'>
+                <div className='image-block'>
+                  <div className='image'></div>
+                  <div className='image-outline'></div>
+                </div>
+                <div className='title'>Georgia</div>
+                <div className='subtitle'>Tbilisi</div>
+              </div>
+              <div className='country-card'>
+                <div className='image-block'>
+                  <div className='image'></div>
+                  <div className='image-outline'></div>
+                </div>
+                <div className='title'>Georgia</div>
+                <div className='subtitle'>Tbilisi</div>
+              </div>
+            </div>
+            <div className='slide'>
+              <div className='country-card'>
+                <div className='image-block'>
+                  <div className='image'></div>
+                  <div className='image-outline'></div>
+                </div>
+                <div className='title'>Georgia</div>
+                <div className='subtitle'>Tbilisi</div>
+              </div>
+              <div className='country-card'>
+                <div className='image-block'>
+                  <div className='image'></div>
+                  <div className='image-outline'></div>
+                </div>
+                <div className='title'>Georgia</div>
+                <div className='subtitle'>Tbilisi</div>
+              </div>
+            </div>
+            <div className='slide'>
+              <div className='country-card'>
+                <div className='image-block'>
+                  <div className='image'></div>
+                  <div className='image-outline'></div>
+                </div>
+                <div className='title'>Georgia</div>
+                <div className='subtitle'>Tbilisi</div>
+              </div>
+              <div className='country-card'>
+                <div className='image-block'>
+                  <div className='image'></div>
+                  <div className='image-outline'></div>
+                </div>
+                <div className='title'>Georgia</div>
+                <div className='subtitle'>Tbilisi</div>
+              </div>
+            </div>
+          </Carousel>
+        </div>
       </div>
     </section>
   </main>
