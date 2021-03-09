6a78ffa199fbeeb9d72f6c459ed22fb6fed23867
bramov <alexey.abramov96@gmail.com>
2021-03-09 15:05:53 +0300

    refactor: add `required` field in schema

--- modules/auth/auth.user.schema.js
+++ modules/auth/auth.user.schema.js
@@ -1,8 +1,14 @@
 const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema ({
-  nickname: String,
-  password: String,
+  nickname: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
 })
 
 const User = model('User', userSchema);
