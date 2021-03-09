4f46cf19afa581eef82f98de7583ef87a66d341c
Iogsotot <ajustusa@gmail.com>
2021-03-10 01:48:14 +0300

    feat: add sight

--- client/src/components/Sight/Sight.tsx
+++ client/src/components/Sight/Sight.tsx
@@ -1,7 +1,23 @@
-import './styles.scss';
-import React, { FC } from 'react';
+import "./sight.scss";
+import React, { FC } from "react";
+import Carousel from "react-elastic-carousel";
+import { SIGHTS } from "../../constants/constants";
 
-const Sight: FC = () => (
-  <div>Sight</div>
-);
+const Sight: FC = () => {
+  const countryName = "belarus";
+  return (
+    <Carousel itemsToScroll={1} itemsToShow={3} isRTL={false} pagination={false} className="slider">
+      {SIGHTS[countryName].map((item) => (
+        <div className="slide" style={{ backgroundImage: `url(${item.bgSrc})` }} key={item.name}>
+          <div className="overlay"></div>
+          <div className="slide__title">{item.name}</div>
+          <div className="rating">
+            {item.rate}
+            <div className="icon--star icon"></div>
+          </div>
+        </div>
+      ))}
+    </Carousel>
+  );
+};
 export default Sight;
