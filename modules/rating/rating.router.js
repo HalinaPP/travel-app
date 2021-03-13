9f053a528d858f021a03785e0d4654fcb3478618
Onberfrest <gregory.moskalev@gmail.com>
2021-03-13 14:50:38 +0300

    feat: rework rating post validate

--- modules/rating/rating.router.js
+++ modules/rating/rating.router.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const wrap = require('../../common/errors/async-error-wrapper');
 const Rating = require('./rating.schema');
-const { InternalServerError } = require('../../common/errors/errors-list');
+const { validateData } = require('../../common/validation/isExist.validation');
+const { InvalidRequestBodyError } = require('../../common/errors/errors-list');
 const { StatusCodes, ReasonPhrases } = require('http-status-codes');
 
 const router = express.Router();
@@ -9,17 +10,17 @@ const router = express.Router();
 router.post(
   '/:id',
   wrap(async ({ body: { placeId, userId, rating: r } }, res) => {
+    if (!validateData(placeId, userId, r)) {
+      throw new InvalidRequestBodyError();
+    }
     const rating = new Rating({
       placeId,
       userId,
       rating: r,
     });
-    try {
-      await rating.save();
-      res.status(StatusCodes.CREATED).send(ReasonPhrases.CREATED);
-    } catch (error) {
-      throw new InternalServerError();
-    }
+
+    await rating.save();
+    res.status(StatusCodes.CREATED).send(ReasonPhrases.CREATED);
   }),
 );
 
