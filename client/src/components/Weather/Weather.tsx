3c33ac28ed6c986615838bd1333df071d1a80ecd
Levendor <myth_of_lavanda@mail.ru>
2021-03-16 01:51:24 +0300

    feat: add weather widget

--- client/src/components/Weather/Weather.tsx
+++ client/src/components/Weather/Weather.tsx
@@ -1,7 +1,43 @@
 import './styles.scss';
-import React, { FC } from 'react';
+import React, { FC, useContext, useEffect, useState } from 'react';
+import { WeatherProps } from './Weather.model';
+import { API_KEY, URL } from './constants';
+import { LanguageContext } from '../../utils/LanguageContext';
 
-const Weather: FC = () => (
-  <div>Weather</div>
-);
+const Weather: FC<WeatherProps> = ({ capital }) => {
+  const { lang: currLang } = useContext(LanguageContext);
+  const [loading, setLoading] = useState(true);
+  const [weatherData, setWeatherData] = useState<any>(null);
+
+  const urlQueryOptions = `?q=${capital}&lang=${currLang}&${API_KEY}&units=metric`;
+
+  useEffect(() => {
+    fetch(URL + urlQueryOptions)
+      .then(res => res.json())
+      .then(data => {
+        setWeatherData(data);
+        setLoading(false);
+      });
+  }, [capital]);
+
+  if (loading) {
+    return <div>--</div>;
+  }
+
+  return (
+    <div className="weather-container">
+      <div className="city-container">
+        <span className="city">{capital}</span>
+        <span className="temperature">
+          {weatherData.main.temp}°C
+        </span>
+      </div>
+      <div className="icon-container">
+        <i className={`weather-icon owf owf-${weatherData.weather[0].id}`}
+          title={weatherData.weather[0].description} />
+      </div>
+    </div>
+  );
+};
+
 export default Weather;
---
+++ client/src/components/Weather/constants.ts
@@ -0,0 +1,2 @@
+export const API_KEY = 'appid=634b0d67ec10e6b80374dfefaba2459c';
+export const URL = 'https://api.openweathermap.org/data/2.5/weather';
