9771cc8f1ef1959a95fe50df159994fc85800b0c
Iogsotot <ajustusa@gmail.com>
2021-03-16 14:01:54 +0300

    feat: minor style fixex / add styles for scroll

--- client/src/components/CountriesNavigator/CountriesNavigator.tsx
+++ client/src/components/CountriesNavigator/CountriesNavigator.tsx
@@ -3,25 +3,26 @@ import { CountriesNavigatorProps } from './CountriesNavigator.model';
 import './countriesNavigator.scss';
 
 const CountriesNavigator: FC<CountriesNavigatorProps> = () => {
+  // мне надо получать массив стран и ссылку на их main bg
+  // так же надо получать current country
   const coountries = [
-    ['georgia', '/images/main_mini.png'],
-    ['russia', '/images/main_mini.png'],
-    ['norway', '/images/main_mini.png'],
-    ['china', '/images/main_mini.png'],
-    ['usa', '/images/main_mini.png'],
-    ['italy', '/images/main_mini.png'],
-    ['germany', '/images/main_mini.png'],
-    ['uar', '/images/main_mini.png'],
+    ['georgia', '/images/main_small.png'],
+    ['russia', '/images/main_small.png'],
+    ['norway', '/images/main_small.png'],
+    ['china', '/images/main_small.png'],
+    ['usa', '/images/main_small.png'],
+    ['italy', '/images/main_small.png'],
+    ['germany', '/images/main_small.png'],
+    ['uar', '/images/main_small.png'],
   ];
   return (
     <aside>
-      <ul className='countries-navigator'>
+      <ul className="countries-navigator">
         {coountries.map(country => (
-
           <li className="countries-navigator__item" key={country[0]}>
             <a href="#" className="link">
               <img src={country[1]} className="icon countries-navigator__img" alt={country[0]} />
-              {country[0]}
+              <p className='country_name'>{country[0]}</p>
             </a>
           </li>
         ))}
