41e9fd7974086d0c118b7a28833404a57d2e08a1
Levendor <myth_of_lavanda@mail.ru>
2021-03-17 02:52:44 +0300

    feat: add fullscreen button

--- client/src/components/SightsList/SightsList.tsx
+++ client/src/components/SightsList/SightsList.tsx
@@ -1,6 +1,7 @@
 import './sightsList.scss';
-import { FC, useCallback, useEffect, useState, useContext } from 'react';
+import { FC, useCallback, useEffect, useState, useContext, useRef } from 'react';
 import Carousel from 'react-elastic-carousel';
+import ImageGallery from 'react-image-gallery';
 import { Ratings, SightProps, Sights } from './SightsList.model';
 import { countRate, getSightRatings } from '../../utils/helpers';
 import Feedback from '../Feedback/Feedback';
@@ -18,6 +19,7 @@ const SightsList: FC<SightProps> = ({ sights, ratings }) => {
   };
   const [isOpen, setIsOpen] = useState(false);
   const [popupData, setPopupData] = useState(initialData);
+  const [isFullscreen, setIsFullscreen] = useState(false);
 
   const openPopup = useCallback(
     (sight: Sights, currRatings: Ratings[]) => {
@@ -39,9 +41,7 @@ const SightsList: FC<SightProps> = ({ sights, ratings }) => {
             <div
               className="slide"
               style={{ backgroundImage: `url(${item.imageUrl})` }}
-              onClick={() => {
-                openPopup(item, sightRatings);
-              }}
+              onClick={() => openPopup(item, sightRatings)}
             >
               <div className="overlay"></div>
               <div className="slide__title">{item.name}</div>
@@ -56,6 +56,33 @@ const SightsList: FC<SightProps> = ({ sights, ratings }) => {
     [sights],
   );
 
+  const switchFullscreen = () => {
+    setIsFullscreen((fullscreen) => !fullscreen);
+  };
+
+  const expandButtonClass = isFullscreen
+    ? 'fullScr fullScr--exit'
+    : 'fullScr fullScr--enter';
+  const expandButtonIconSrc = isFullscreen
+    ? '/icons/fs--exit.png'
+    : '/icons/fs.png';
+  const expandButton = (<div className={expandButtonClass}>
+    <img src={expandButtonIconSrc} alt="full screen"
+      onClick={switchFullscreen} />
+  </div>);
+  const fullscreenElement = document.getElementById('wrapper');
+
+  const toggleFullscreen = () => {
+    if (isFullscreen) {
+      fullscreenElement?.requestFullscreen();
+    } else {
+      document.exitFullscreen()
+        .catch(err => Promise.resolve(err));
+    }
+  };
+
+  useEffect(() => toggleFullscreen(), [isFullscreen]);
+
   useEffect(() => {}, [popupData]);
 
   function getSliderCount() {
@@ -69,24 +96,25 @@ const SightsList: FC<SightProps> = ({ sights, ratings }) => {
     return 1;
   }
 
-  // function fullScreenToggle(move: string) {
-  //   if (move === 'in') {
-  //   }
-  // }
+  const carousel =
+    <Carousel className="slider" isRTL={false}
+      pagination={false} itemsToScroll={1}
+      itemsToShow={getSliderCount()} >
+      {getSightsList()}
+    </Carousel>;
+
+  const images = (
+    <div className="slider">
+      {getSightsList()}
+    </div>
+  );
+
   return (
     <section className="sight-slider" id="sight">
-      <div className="wrapper">
+      <div className="wrapper" id="wrapper">
         <h3 className="subtitle">{langsInfo.sights}</h3>
-        <div className="fullScr fullScr--enter">
-          <img src="/icons/fs.png" alt="full screen" />
-        </div>
-
-        <div className="fullScr fullScr--exit hide">
-          <img src="/icons/fs--exit.png" alt="full screen" />
-        </div>
-        <Carousel itemsToScroll={1} itemsToShow={getSliderCount()} isRTL={false} pagination={false} className="slider">
-          {getSightsList()}
-        </Carousel>
+        {expandButton}
+        {isFullscreen ? images : carousel}
       </div>
       <Feedback isOpen={isOpen} setIsOpen={setIsOpen} sight={popupData.sight} ratings={popupData.ratings} />
     </section>
