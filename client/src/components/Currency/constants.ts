08e4d689cfd09eff5c1db5f4b2d3e4f8cdf96554
halinapp <galunikipp@gmail.com>
2021-03-17 23:40:00 +0300

    fix: images

--- client/src/components/Currency/constants.ts
+++ client/src/components/Currency/constants.ts
@@ -1,7 +1,7 @@
 import { TCurrencyTitle } from './Currency.model';
 
 const API_KEY = 'ad62f5f58d475b612757e8490c150c56';
-const BASE_URL = `http://data.fixer.io/api/latest?access_key=${API_KEY}&base=`;
+const BASE_URL = `https://data.fixer.io/api/latest?access_key=${API_KEY}&base=`;
 const BASE_CURRENCIES = '&symbols=RUB,EUR,USD';
 const CURRENCIES = ['BYN', 'CAD', 'CHF', 'EUR', 'GEL', 'ISK', 'KZT', 'MXN', 'NOK', 'RON'];
 
