26e54acca69fbc02dd52aa184554f6822960761e
halinapp <galunikipp@gmail.com>
2021-03-17 23:59:19 +0300

    fix

--- client/src/constants/constants.ts
+++ client/src/constants/constants.ts
@@ -2,10 +2,11 @@ export const imagesPath = './src/assets/images/';
 export const LANGS = { en: 'en', ru: 'ru', bg: 'bg' };
 
 export const HEADER_JSON = { 'Content-Type': 'application/json' };
-/*
+
 export const SERVER_HOST = 'https://travel-be-31.herokuapp.com';
-*/
+/*
 export const SERVER_HOST = 'http://localhost:3005';
+*/
 export const API_COUNTRIES_URLS = `${SERVER_HOST}/countries`;
 export const API_AUTH_URLS = `${SERVER_HOST}/auth`;
 export const API_COUNTRY_URLS = `${SERVER_HOST}/country`;
